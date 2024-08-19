4736354b43ef40c2542972332079ad50fd12da9c
Adeel Qayum <43975177+adeelqayum@users.noreply.github.com>
2024-08-18 21:31:42 -0500

    (U) Finished analysis. Might come back in the future to build up some charts.

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ class Region(Enum):
 
 #member variables
 patients = []
+male_string = "MALE"
+female_string = "FEMALE"
     
 def get_region_enum(region_string):
     match region_string.upper():
@@ -36,12 +38,12 @@ def get_region_enum(region_string):
 def create_patient_objects(list_records):
     for patient_info in list_records:
         age = int(patient_info["age"])
-        sex = patient_info["sex"]
+        sex = patient_info["sex"].upper()
         bmi = patient_info["bmi"]
-        children = patient_info["children"]
+        children = int(patient_info["children"])
         smoker = False if patient_info["smoker"].upper() == "NO" else True
         region = get_region_enum(patient_info["region"])
-        charges = patient_info["charges"]
+        charges = round(float(patient_info["charges"]), 2)
         patient_instance = PatientInformation(age, sex, bmi, children, smoker, region, charges)
         patients.append(patient_instance)
 
@@ -58,10 +60,33 @@ def perform_analysis():
     print(f"There are a total of {num_rows} medical records in the dataset.")
     
     age_sum = 0
+    total_num_children = 0
+    num_males = 0
+    num_male_smokers = 0
+    num_females = 0
+    num_female_smokers = 0
     for patient in patients:
         age_sum += patient.age
+        total_num_children += patient.children
+        sex = patient.sex
+        if sex == male_string:
+            num_males += 1
+            if patient.smoker == True:
+                num_male_smokers += 1
+        elif sex == female_string:
+            num_females += 1
+            if patient.smoker == True:
+                num_female_smokers += 1
+        
     average_age = round(age_sum/num_rows, 2)
     print(f"The average age of the dataset is {average_age}")
+    average_num_children = round(total_num_children/num_rows, 2)
+    print(f"The average number of children per patient is {average_num_children}")
+    percent_males_smoker = round((num_male_smokers/num_males) * 100, 2)
+    percent_females_smoker = round((num_female_smokers/num_females) * 100, 2)
+    print(f"The percent of male smokers is {percent_males_smoker}%")
+    print(f"The percent of male smokers is {percent_females_smoker}%")
+
         
 
 load_data_from_file('insurance.csv')
